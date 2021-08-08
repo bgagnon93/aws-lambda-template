64120ef567b0b41692dc6a659a105d9aed82c39f
bgagnon93 <38666646+bgagnon93@users.noreply.github.com>
2021-08-08 13:45:57 -0400

    fix lambda_handler

--- lambda_template/app.py
+++ lambda_template/app.py
@@ -1,6 +1,6 @@
 import json
 
-def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[Str, Any]:
+def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
